2ea45c4c2bf1c88bb744c7e17b1b87eee276e89c
Nacho Casado <jcasado@paradigmadigital.com>
2018-05-21 12:38:11 +0200

    Initial commit

---
+++ src/main/java/com/personal/kafka/springkafkastreams/SpringKafkaStreamsApplication.java
@@ -0,0 +1,12 @@
+package com.personal.kafka.springkafkastreams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKafkaStreamsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringKafkaStreamsApplication.class, args);
+	}
+}
